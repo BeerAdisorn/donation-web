4b25e10df9934ffe2d3f17c8e2e761334c3e8806
BeerAdisorn <adisornnakyim2@gmail.com>
2019-12-11 01:48:26 +0700

    ทำ login & logout

---
+++ public/FirebaseConfig.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD7L_D6s9qX0f2Vg2Dczaso3RDR8VFBucQ",
+    authDomain: "donationapplication-1c1bd.firebaseapp.com",
+    databaseURL: "https://donationapplication-1c1bd.firebaseio.com",
+    projectId: "donationapplication-1c1bd",
+    storageBucket: "donationapplication-1c1bd.appspot.com",
+    messagingSenderId: "1033576244361",
+    appId: "1:1033576244361:web:336a300957214e9ff7d258",
+    measurementId: "G-3SVZYGN924"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
